a5c26a1037710c2d0445937016de741a9e7504d6
RossLitz <codingclasses.049@gmail.com>
2015-06-09 22:59:25 -0500

    User Registration

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
-source 'https://rubygems.org'
+ source 'https://rubygems.org'
 
 gem 'rails', '4.2.0'
 gem 'sqlite3'
 gem 'simple_form'
 gem 'haml_rails'
+gem 'bcrypt'
 
 # Assets
 gem 'sass-rails', '~> 5.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609174633) do
+ActiveRecord::Schema.define(version: 20150610024937) do
 
   create_table "entries", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
